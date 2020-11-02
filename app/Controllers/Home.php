8767198f2533a5123f3d3b1e2c65ec6af004714b
agusarya05 <agus.arya@podomorocitydelimedan.com>
2020-11-02 12:25:14 +0700

    Menambahkan file bootstrap Admin-LTE 3.0.5

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,10 +1,15 @@
-<?php namespace App\Controllers;
+<?php
+
+namespace App\Controllers;
 
 class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+		// return view('welcome_message');
+		echo view('header');
+		echo view('body');
+		echo view('footer');
 	}
 
 	//--------------------------------------------------------------------
